d16b3a10ae307e6534ddf83133ae9f521540fa55
FreddMx <freddlopez261@gmail.com>
2023-11-16 00:48:50 -0600

    added source

---
+++ Config/Config.php
@@ -0,0 +1,105 @@
+<?php 
+	const BASE_URL = "http://localhost/tienda_virtual";
+	//const BASE_URL = "https://abelosh.com/tiendavirtual";
+
+	//Zona horaria
+	date_default_timezone_set('America/Guatemala');
+
+	//Datos de conexión a Base de Datos
+	const DB_HOST = "localhost";
+	const DB_NAME = "db_tiendavirtual";
+	const DB_USER = "root";
+	const DB_PASSWORD = "";
+	const DB_CHARSET = "utf8";
+
+	//Para envío de correo
+	const ENVIRONMENT = 1; // Local: 0, Produccón: 1;
+
+	//Deliminadores decimal y millar Ej. 24,1989.00
+	const SPD = ".";
+	const SPM = ",";
+
+	//Simbolo de moneda
+	const SMONEY = "$";
+	const CURRENCY = "USD";
+
+	//Api PayPal
+	//SANDBOX PAYPAL
+	const URLPAYPAL = "https://api-m.sandbox.paypal.com";
+	const IDCLIENTE = "";
+	const SECRET = "";
+	//LIVE PAYPAL
+	//const URLPAYPAL = "https://api-m.paypal.com";
+	//const IDCLIENTE = "";
+	//const SECRET = "";
+
+	//Datos envio de correo
+	const NOMBRE_REMITENTE = "TuxMarket";
+	const EMAIL_REMITENTE = "no-reply@tuxmarket.com";
+	const NOMBRE_EMPESA = "TuxMarket";
+	const WEB_EMPRESA = "www.tuxmarket.shop";
+
+	const DESCRIPCION = "¡Siempe a tu servicio!";
+	const SHAREDHASH = "TuxMarket";
+
+	//Datos Empresa
+	const DIRECCION = "Boulevard Belisario Domínguez, Km. 1081, Sin Número, Terán, Tuxtla Gutiérrez, Chiapas, México, 29050.";
+	const TELEMPRESA = "+(961)8214300";
+	const WHATSAPP = "+52 961 821 4300";
+	const EMAIL_EMPRESA = "www.tuxmarket.shop";
+	const EMAIL_PEDIDOS = "www.tuxmarket.shop"; 
+	const EMAIL_SUSCRIPCION = "www.tuxmarket.shop";
+	const EMAIL_CONTACTO = "www.tuxmarket.shop";
+
+	const CAT_SLIDER = "1,2,3";
+	const CAT_BANNER = "4,5,6";
+	const CAT_FOOTER = "1,2,3,4,5";
+
+	//Datos para Encriptar / Desencriptar
+	const KEY = 'abelosh';
+	const METHODENCRIPT = "AES-128-ECB";
+
+	//Envío
+	const COSTOENVIO = 5;
+
+	//Módulos
+	const MDASHBOARD = 1;
+	const MUSUARIOS = 2;
+	const MCLIENTES = 3;
+	const MPRODUCTOS = 4;
+	const MPEDIDOS = 5;
+	const MCATEGORIAS = 6;
+	const MSUSCRIPTORES = 7;
+	const MDCONTACTOS = 8;
+	const MDPAGINAS = 9;
+
+	//Páginas
+	const PINICIO = 1;
+	const PTIENDA = 2;
+	const PCARRITO = 3;
+	const PNOSOTROS = 4;
+	const PCONTACTO = 5;
+	const PPREGUNTAS = 6;
+	const PTERMINOS = 7;
+	const PSUCURSALES = 8;
+	const PERROR = 9;
+
+	//Roles
+	const RADMINISTRADOR = 1;
+	const RSUPERVISOR = 2;
+	const RCLIENTES = 3;
+
+	const STATUS = array('Completo','Aprobado','Cancelado','Reembolsado','Pendiente','Entregado');
+
+	//Productos por página
+	const CANTPORDHOME = 8;
+	const PROPORPAGINA = 4;
+	const PROCATEGORIA = 4;
+	const PROBUSCAR = 4;
+
+	//REDES SOCIALES
+	const FACEBOOK = "https://www.facebook.com/tuxmarket";
+	const INSTAGRAM = "https://www.instagram.com/tuxmarket/";
+	
+
+ ?>
---
+++ Views/Contacto/contacto.php
@@ -0,0 +1,116 @@
+<?php 
+headerTienda($data);
+$banner = $data['page']['portada'];
+$idpagina = $data['page']['idpost'];
+
+ ?>
+<script>
+ 	document.querySelector('header').classList.add('header-v4');
+ </script>
+<!-- Title page -->
+<section class="bg-img1 txt-center p-lr-15 p-tb-92" style="background-image: url(<?= $banner ?>);">
+	<h2 class="ltext-105 cl0 txt-center">
+		Contacto
+	</h2>
+</section>
+
+<?php
+	if(viewPage($idpagina)){	
+ ?>
+<!-- Content page -->
+<section class="bg0 p-t-104 p-b-116">
+	<div class="container">
+		<div class="flex-w flex-tr">
+			<div class="size-210 bor10 p-lr-70 p-t-55 p-b-70 p-lr-15-lg w-full-md">
+				<form id="frmContacto">
+					<h4 class="mtext-105 cl2 txt-center p-b-30">
+						Enviar un mensaje
+					</h4>
+
+					<div class="bor8 m-b-20 how-pos4-parent">
+						<input class="stext-111 cl2 plh3 size-116 p-l-62 p-r-30" type="text" id="nombreContacto" name="nombreContacto" placeholder="Nombre completo">
+						<img class="how-pos4 pointer-none" src="<?= media() ?>/tienda/images/icons/icon-name.png" alt="ICON" style="width: 28px;">
+					</div>
+
+					<div class="bor8 m-b-20 how-pos4-parent">
+						<input class="stext-111 cl2 plh3 size-116 p-l-62 p-r-30" type="text" id="emailContacto" name="emailContacto" placeholder="Correo electrónico">
+						<img class="how-pos4 pointer-none" src="<?= media() ?>/tienda/images/icons/icon-email.png" alt="ICON">
+					</div>
+
+					<div class="bor8 m-b-30">
+						<textarea class="stext-111 cl2 plh3 size-120 p-lr-28 p-tb-25" id="mensaje" name="mensaje" placeholder="Cual es tu pregunta o mensaje?"></textarea>
+					</div>
+
+					<button class="flex-c-m stext-101 cl0 size-121 bg3 bor1 hov-btn3 p-lr-15 trans-04 pointer">
+						Enviar
+					</button>
+				</form>
+			</div>
+
+			<div class="size-210 bor10 flex-w flex-col-m p-lr-93 p-tb-30 p-lr-15-lg w-full-md">
+				<div class="flex-w w-full p-b-42">
+					<span class="fs-18 cl5 txt-center size-211">
+						<span class="lnr lnr-map-marker"></span>
+					</span>
+
+					<div class="size-212 p-t-2">
+						<span class="mtext-110 cl2">
+							Dirección
+						</span>
+
+						<p class="stext-115 cl6 size-213 p-t-18">
+							<?= DIRECCION ?>
+						</p>
+					</div>
+				</div>
+
+				<div class="flex-w w-full p-b-42">
+					<span class="fs-18 cl5 txt-center size-211">
+						<span class="lnr lnr-phone-handset"></span>
+					</span>
+
+					<div class="size-212 p-t-2">
+						<span class="mtext-110 cl2">
+							Teléfono
+						</span>
+
+						<p class="stext-115 cl1 size-213 p-t-18">
+
+							<a class="" href="tel:<?= TELEMPRESA ?>"><?= TELEMPRESA ?></a>
+						</p>
+					</div>
+				</div>
+
+				<div class="flex-w w-full">
+					<span class="fs-18 cl5 txt-center size-211">
+						<span class="lnr lnr-envelope"></span>
+					</span>
+
+					<div class="size-212 p-t-2">
+						<span class="mtext-110 cl2">
+							E-mail
+						</span>
+
+						<p class="stext-115 cl1 size-213 p-t-18">
+							<a class="" href="mailto:<?= EMAIL_EMPRESA ?>"><?= EMAIL_EMPRESA ?>
+						</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>	
+<?php 
+		echo $data['page']['contenido'];
+	}else{
+?>
+<div>
+	<div class="container-fluid py-5 text-center" >
+		<img src="<?= media() ?>/images/construction.png" alt="En construcción">
+		<h3>Estamos trabajando para usted.</h3>
+	</div>
+</div>
+<?php 
+	}
+	footerTienda($data);
+?>
