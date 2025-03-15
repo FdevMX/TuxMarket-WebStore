ad0639736da69c7af0d714cf510e5dc272f4062a
fdevmx <filesforcloud.fdev.mx@gmail.com>
2025-03-15 03:04:17 -0600

    add Json element for dashboard

--- Libraries/Core/Controllers.php
+++ Libraries/Core/Controllers.php
@@ -2,6 +2,9 @@
 	
 	class Controllers
 	{
+		protected $views;
+		protected $model;
+		
 		public function __construct()
 		{
 			$this->views = new Views();
@@ -17,6 +20,29 @@
 				require_once($routClass);
 				$this->model = new $model();
 			}
+		}
+		
+        // Método para enviar respuestas JSON correctamente
+
+		protected function sendJson($data)
+		{
+			// Asegurarse de que no haya salida previa
+			if (headers_sent()) {
+				// Si ya se enviaron los encabezados, simplemente emitimos el JSON
+				echo json_encode($data, JSON_UNESCAPED_UNICODE);
+			} else {
+				// Si podemos enviar encabezados, limpiamos cualquier salida previa
+				if (ob_get_length() > 0) {
+					ob_clean();
+				}
+				
+				// Establecer encabezados
+				header('Content-Type: application/json');
+				
+				// Enviar respuesta JSON
+				echo json_encode($data, JSON_UNESCAPED_UNICODE);
+			}
+			die();
 		}
 	}
 
